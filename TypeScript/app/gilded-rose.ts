6f83bc9311fb5e3e0de8f6c28905f189efee9de1
sairasarashaji <sairasarashaji05@gmail.com>
2024-02-20 10:31:25 +1300

    refactored updateQuality method

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -23,51 +23,54 @@ export class GildedRose {
 
   updateQuality() {
     for (let item of this.items) {
-      if (item.name != this.agedBrie && item.name != this.backstagePasses) {
-        if (item.quality > 0) {
-          if (item.name != this.sulfuras) {
-            item.quality = item.quality - 1
-          }
-        }
-      } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1
-          if (item.name == this.backstagePasses) {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1
-              }
-            }
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1
-              }
-            }
-          }
-        }
-      }
-      if (item.name != this.sulfuras) {
-        item.sellIn = item.sellIn - 1;
-      }
-      if (item.sellIn < 0) {
-        if (item.name != this.agedBrie) {
-          if (item.name != this.backstagePasses) {
-            if (item.quality > 0) {
-              if (item.name != this.sulfuras) {
-                item.quality = item.quality - 1
-              }
-            }
-          } else {
-            item.quality = item.quality - item.quality
-          }
-        } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1
-          }
-        }
+      if(item.name == this.agedBrie){
+        this.updateAgedBrieQuality(item)
+      } else if(item.name == this.backstagePasses){
+          this.updateBackstagePassesQuality(item)
+      } else if(item.name == this.sulfuras) {
+          // no change in quality.
+      } else{
+        this.updateNormalItemQuality(item)
       }
     }
 
     return this.items;
+  }
+
+  updateAgedBrieQuality(item: Item){
+    if (item.quality < 50) {
+      item.quality = item.quality + 1
+    }
+    item.sellIn = item.sellIn - 1;
+    if (item.sellIn < 0 && item.quality < 50) {
+        item.quality = item.quality + 1
+    }
+  }
+
+  updateBackstagePassesQuality(item:Item){
+    if (item.quality < 50) {
+      item.quality = item.quality + 1
+
+      if (item.sellIn < 11 && item.quality < 50) {
+          item.quality = item.quality + 1
+      }
+      if (item.sellIn < 6 && item.quality < 50) {
+          item.quality = item.quality + 1
+      }
+    }
+    item.sellIn = item.sellIn - 1
+    if (item.sellIn < 0) {
+        item.quality = 0
+    }
+  }
+
+  updateNormalItemQuality(item: Item){
+    if (item.quality > 0) {
+      item.quality = item.quality - 1
+    }
+    item.sellIn = item.sellIn - 1;
+    if (item.sellIn < 0 && item.quality > 0) {
+            item.quality = item.quality - 1
+    }
   }
 }
